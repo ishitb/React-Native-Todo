287c5f98a499e0722384b673dbf6e6868045f0b5
Ishit Beswal <ib370@snu.edu.in>
2020-04-23 13:27:32 +0530

    updated state to take todo list array

--- TODO-APP/App.js
+++ TODO-APP/App.js
@@ -1,13 +1,49 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, ImageBackground, Dimensions } from 'react-native';
-import image from "./assets/background.jpg"
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+  TextInput,
+} from "react-native";
+import image from "./assets/background.jpg";
 
-export default class App extends Component{
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    todos: ["One", "Two", "Three"],
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground source={image} style={styles.backg}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <View style={styles.mainApp}>
+            <View>
+              <TextInput style={styles.input} />
+              <View style={styles.presentTodos}>
+                {this.state.todos.map((todo, index) => {
+                  return (
+                    <React.Fragment>
+                      <Text style={styles.todos} key={index}>
+                        {todo}
+                      </Text>
+                      {index < this.state.todos.length - 1 ? (
+                        <View style={styles.break}></View>
+                      ) : null}
+                    </React.Fragment>
+                  );
+                })}
+              </View>
+            </View>
+            <Text style={styles.footer}>
+              Notes App with Firebase Integration
+            </Text>
+          </View>
         </ImageBackground>
       </View>
     );
@@ -17,14 +53,51 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   backg: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
-    width: Dimensions.get("window").width
-  }
+    width: Dimensions.get("window").width,
+  },
+  mainApp: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    height: Dimensions.get("screen").height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 5,
+    paddingHorizontal: 10,
+    width: 350,
+  },
+  footer: {
+    backgroundColor: "#444",
+    padding: 10,
+    fontSize: 20,
+    fontWeight: "500",
+    textAlign: "center",
+    color: "#fff",
+    width: 350,
+  },
+  presentTodos: {
+    backgroundColor: "#444",
+    borderBottomColor: "#fff",
+    borderBottomWidth: 1,
+  },
+  todos: {
+    padding: 10,
+    fontSize: 18,
+    color: "#fff",
+  },
+  break: {
+    backgroundColor: "#fff",
+    height: 1,
+    width: 150,
+    alignSelf: "center",
+  },
 });
