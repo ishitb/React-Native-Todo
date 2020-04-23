e36c344625d6482e30c8a439e85075aaec2f1d4d
Ishit Beswal <ib370@snu.edu.in>
2020-04-23 20:35:08 +0530

    delete todo

--- TODO-APP/App.js
+++ TODO-APP/App.js
@@ -1,19 +1,159 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+  TextInput,
+  TouchableHighlight,
+} from "react-native";
+import image from "./assets/background.jpg";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.newTodo = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  state = {
+    todos: ["One", "Two", "Three", "Four"],
+    inputVal: "",
+    deleteColor: "#fff"
+  };
+
+  handleSubmit = () => {
+    var newTodo = this.state.inputVal;
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+      inputVal: "",
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      inputVal: e,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ImageBackground source={image} style={styles.backg}>
+          <View style={styles.mainApp}>
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Add New Todo"
+                // ref={this.newTodo}
+                value={this.state.inputVal}
+                onChangeText={this.handleChange}
+                onSubmitEditing={this.handleSubmit}
+              />
+              <View style={styles.presentTodos}>
+                {this.state.todos.map((todo, index) => {
+                  return (
+                    <React.Fragment key={index}>
+                      <View style={styles.list}>
+                        <Text style={styles.todos}>{todo}</Text>
+                        <TouchableHighlight
+                          underlayColor="rgba(0, 0, 0, 0)"
+                          activeOpacity={0.4}
+                          onPress={() => {
+                            this.setState({
+                              todos: [
+                                ...this.state.todos.filter(
+                                  (todo) =>
+                                    this.state.todos.indexOf(todo) != index
+                                ),
+                              ],
+                            });
+                          }}
+                        >
+                          <Text style={styles.deleteButton}>
+                            <AntDesign
+                              name="minuscircleo"
+                              size={25}
+                              color="green"
+                            />
+                          </Text>
+                        </TouchableHighlight>
+                      </View>
+                      {index < this.state.todos.length - 1 ? (
+                        <View style={styles.break}></View>
+                      ) : null}
+                    </React.Fragment>
+                  );
+                })}
+              </View>
+            </View>
+            <Text style={styles.footer}>
+              Notes App with Firebase Integration
+            </Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+  },
+  mainApp: {
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    height: Dimensions.get("screen").height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 5,
+    paddingHorizontal: 10,
+    width: 350,
+  },
+  footer: {
+    backgroundColor: "#444",
+    padding: 10,
+    fontSize: 20,
+    fontWeight: "500",
+    textAlign: "center",
+    color: "#fff",
+    width: 350,
+  },
+  presentTodos: {
+    backgroundColor: "#444",
+    borderBottomColor: "#fff",
+    borderBottomWidth: 1,
+  },
+  todos: {
+    padding: 10,
+    fontSize: 18,
+    color: "#fff",
+  },
+  break: {
+    backgroundColor: "#fff",
+    height: 1,
+    width: 150,
+    alignSelf: "center",
+  },
+  list: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  deleteButton: {
+    padding: 10,
   },
 });
